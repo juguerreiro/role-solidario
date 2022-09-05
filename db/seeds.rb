bf1c5e68b05d9d66e24fa04296b7f6f549f7f5e9
Isabela Meyer <56268057+meyerisabela@users.noreply.github.com>
2022-09-05 18:10:08 -0300

    Seed: added user and report news to seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+ReportNew.destroy_all
+
+maiko = User.create!(admin: true, first_name: "Maiko", email: "maiko@maiko.com", password: "12345678")
+isa = User.create!(first_name: "Isabela", last_name: "Meyer", email: "isa@isa.com", password: "12345678")
+julia = User.create!(first_name: "Julia", last_name: "Guerreiro", email: "julia@julia.com", password: "12345678")
+
+ReportNew.create!(description: Faker::Lorem.sentence(word_count: 30), title: Faker::Lorem.sentence(word_count: 5), user_id:  maiko.id)
+ReportNew.create!(description: Faker::Lorem.sentence(word_count: 30), title: Faker::Lorem.sentence(word_count: 5), user_id:  maiko.id)
+ReportNew.create!(description: Faker::Lorem.sentence(word_count: 30), title: Faker::Lorem.sentence(word_count: 5), user_id:  maiko.id)
+ReportNew.create!(description: Faker::Lorem.sentence(word_count: 30), title: Faker::Lorem.sentence(word_count: 5), user_id:  maiko.id)
+ReportNew.create!(description: Faker::Lorem.sentence(word_count: 30), title: Faker::Lorem.sentence(word_count: 5), user_id:  maiko.id)
+ReportNew.create!(description: Faker::Lorem.sentence(word_count: 30), title: Faker::Lorem.sentence(word_count: 5), user_id:  maiko.id)
