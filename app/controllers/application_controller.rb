dd9528172567efe93529c7f591180d0ef0a1174f
Julia Guerreiro <juguerreiro123@gmail.com>
2022-09-05 18:48:31 -0300

    Controller: remove updated upstream.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< Updated upstream
   # before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -11,7 +10,5 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
-=======
   before_action :authenticate_user!
->>>>>>> Stashed changes
 end
