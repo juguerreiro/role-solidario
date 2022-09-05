15aec0c0c9b64bb0475d3dd8d44abab8556fe9c5
Julia Guerreiro <juguerreiro123@gmail.com>
2022-09-05 18:49:24 -0300

    Routes: remove stashed warning.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
   root to: "donations#index"
   resources :report_news, only: [:index]
-=======
->>>>>>> Stashed changes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-<<<<<<< Updated upstream
   resources :donations, only: %i[new create]
-=======
   resources :donations, only: %i[new create make_your_donation]
->>>>>>> Stashed changes
+  resources :donations, only: %i[new create make_your_donation]
 end
