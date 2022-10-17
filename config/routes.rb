d65dcc12750c7a09e4ce6fd93fe58a8444302f22
Julia Guerreiro <juguerreiro123@gmail.com>
2022-10-17 12:36:30 -0300

    Modal: modal working for both, the checkbox and the value entered

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :donations, only: %i[new create make_your_donation]
+  resources :donations, only: %i[new create]
 end
